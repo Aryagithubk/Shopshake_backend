1164afd115f3437b3674e0160b0454ed103d0dfe
KRISHA ARYA <117719872+Aryagithubk@users.noreply.github.com>
2025-05-26 00:04:58 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const { app } = require("./index.js");
-const { connectDb } = require("./config/db.js");
+const { connectDb } = require("./config/db");
 
 const port = 5454;
-app.listen(PORT, async () => {
+app.listen(process.env.PORT, async () => {
   await connectDb();
-  console.log("ecommerce api listing on port ", PORT);
+  console.log("ecommerce api listing on port ", process.env.PORT);
 });
